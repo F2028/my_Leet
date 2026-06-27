9212ca7c347b4ce737d95c2ee1c9812ddcf802c0
F2028 <nawakit.n184@gmail.com>
2026-06-27 12:33:37 +0700

    myfirstleet

---
+++ 02-palindrome/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+            if (x < 0){
+                return false;
+            }
+            long long a = 0;
+            int b = x;
+           while (x != 0){
+              int digit = x % 10;       
+               a = a * 10 + digit;      
+              x = x / 10;
+            }
+            return {b == a};
+    }
+};
