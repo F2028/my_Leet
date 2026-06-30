bdf9f2dd587711f09da5b8a980d6fb3953b7eb68
F2028 <nawakit.n184@gmail.com>
2026-06-30 18:12:36 +0700

    commonprefix

---
+++ common-prefix/common-prefix.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()){
+            return "";
+        }
+            for (int i = 0 ; ; i++){                        
+                for (int j = 0; j < strs.size(); j++){          
+                        if (i >= strs[j].size() || strs[j][i] != strs[0][i]){
+                            return strs[0].substr(0,i);
+                        }
+                }     
+            }
+            return strs[0];            
+    }
+};
