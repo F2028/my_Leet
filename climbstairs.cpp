f7c2ee892df349c863a5f87863cce546c4b9d36d
F2028 <nawakit.n184@gmail.com>
2026-07-23 23:40:45 +0700

    climbstairs

---
+++ climbstairs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <vector>
+#include <algorithm>
+using namespace std; 
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1){
+            return 1;
+        }
+      vector<int> ways(n+1);        //สร้าง variableให้ตรงกับ index
+      ways[1] = 1;                  // เรารู้อยู่ละอันนี้ คือ step เเรก
+      ways[2] = 2;                  // step 2
+        for (int i = 3; i < ways.size();i++){       // ให้มันวนเท่า n && ways.size() = n+1   i < n+1 
+            ways[i] = ways[i-1] + ways[i-2];        
+        }
+        return ways[n];
+    }
+};
