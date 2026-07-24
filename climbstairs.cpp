d58f689d85cfc9336c2c3bb1a9675e5d2cf5407d
F2028 <nawakit.n184@gmail.com>
2026-07-24 14:57:11 +0700

    mySqrt

--- climbstairs.cpp
+++ climbstairs.cpp
@@ -6,7 +6,7 @@
 using namespace std; 
 class Solution {
 public:
-    int climbStairs(int n) {
+    int climbStairs(int n) {        // dp = dynamic programming
         if (n == 1){
             return 1;
         }
@@ -14,7 +14,7 @@ public:
       ways[1] = 1;                  // เรารู้อยู่ละอันนี้ คือ step เเรก
       ways[2] = 2;                  // step 2
         for (int i = 3; i < ways.size();i++){       // ให้มันวนเท่า n && ways.size() = n+1   i < n+1 
-            ways[i] = ways[i-1] + ways[i-2];        
+            ways[i] = ways[i-1] + ways[i-2];        // ways[i] = ways[2-1] + ways[2-2] = ways[1]
         }
         return ways[n];
     }
---
+++ mySqrt.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <vector>
+#include <algorithm>
+using namespace std; 
+class Solution {
+public:
+    int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+        while (left <= right){
+           int mid = (left + right) / 2;      // ให้มันคำนวณได้หลายครั้ง
+            if (mid * mid < x){ 
+                left = mid + 1;           //move pointer to right
+            } else if (mid * mid > x){
+                right = mid - 1;            //move pointer to left
+            } else return mid;
+            
+        }
+        return right;           // return right เพราะ ค่า - มันจะพอดี
+    }
+};
