8cb616cc83ff941b96b3d82c88864c0a62e06e2a
F2028 <nawakit.n184@gmail.com>
2026-08-06 00:47:12 +0700

    144 preordertraversal

---
+++ 144preordertraversal.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std; 
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+class Solution {
+public:
+    void helper (TreeNode* root , vector <int>& result){    //เราส่ง vector ไปเเค่เก็บค่าเฉยๆไม่จำเป็นต้องมี data           
+            if (root == nullptr){
+                return;
+            }     
+            result.push_back(root->val);                    //เพราะมัน pushเข้า vector เปล่าอยู่เเล้ว
+             helper(root->left,result);                     // recursion เช้ค left
+             helper(root->right,result);                    // recursion เช้ค right
+            
+    }
+    vector<int> preorderTraversal(TreeNode* root) {      
+            vector <int> ans;   
+            helper(root,ans);
+            return ans;
+    }
+};
